94a1cc7288ec41a4ea3541f94d67ba0badf04993
lucas <lucas.alek@gmail.com>
2018-03-21 11:31:46 -0500

    added Year Table
    also added to active admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321161119) do
+ActiveRecord::Schema.define(version: 20180321162859) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 20180321161119) do
     t.string "model"
     t.string "make"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "years", force: :cascade do |t|
+    t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
